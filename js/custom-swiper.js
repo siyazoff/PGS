d266a69a656fa79d5839a3fb70c0522276be8a55
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-02-08 14:44:11 +0600

    pgs ends

--- js/custom-swiper.js
+++ js/custom-swiper.js
@@ -15,3 +15,66 @@ const homeSectionSwiper = new Swiper(".swiper-home", {
     prevEl: ".home-swiper-prev",
   },
 });
+
+const experienceSectionSwiper = new Swiper(".swiper-experience", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  speed: 800,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".experience-swiper-next",
+    prevEl: ".experience-swiper-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {},
+  },
+});
+
+const partnersSectionSwiper = new Swiper(".swiper-partners", {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 24,
+  speed: 3000,
+  freeMode: true,
+  freeModeMomentum: false,
+
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {},
+  },
+});
+
+const goalsSectionSwiper = new Swiper(".swiper-goals", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  speed: 800,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-goals-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-goals-next",
+    prevEl: ".swiper-goals-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {},
+  },
+});
--- js/main.js
+++ js/main.js
@@ -4,5 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
   burger.addEventListener("click", function () {
     popup.classList.toggle("is-active");
+    this.classList.toggle("is-active");
+  });
+
+  document.querySelectorAll(".advantages__item").forEach(function (item) {
+    item.addEventListener("mouseenter", function () {
+      const image = item.querySelector(".advantages__hover_img");
+      image.style.opacity = "1";
+
+      const mouseMoveHandler = function (e) {
+        const rect = item.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        image.style.transform = `translate(${x - 500}px, ${y}px)`;
+      };
+
+      item.addEventListener("mousemove", mouseMoveHandler);
+
+      item.addEventListener(
+        "mouseleave",
+        function () {
+          image.style.opacity = "0";
+          item.removeEventListener("mousemove", mouseMoveHandler);
+        },
+        { once: true }
+      );
+    });
   });
 });
